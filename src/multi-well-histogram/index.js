1f6c253de8af21e3d22d83bf13579d87f8191e39
DTM1012 <modaouet1012@gmail.com>
2019-06-01 10:52:36 +0700

    update

--- src/multi-well-histogram/index.js
+++ src/multi-well-histogram/index.js
@@ -29,18 +29,68 @@ function multiWellHistogramController($scope, $timeout, $element, wiToken, wiApi
         }).catch(e => console.error(e));
     }
     
-    this.getCurves = function(well) {
-        
+    function getCurvesInWell(well) {
+        let curves = [];
+        well.datasets.forEach(dataset => {
+            curves.push(...dataset.curves);
+        });
+        return curves;
+    }
+
+    function getFamilyInWell(well) {
+        let curves = getCurvesInWell(well);
+        let familyList = curves.map(c => wiApi.getFamily(c.idFamily));
+        return familyList;
     }
     this.$onInit = function () {
+        $scope.$watch(() => (self.wells), () => {
+            getTree(function() {
+                getSelectionList(self.selectionType, self.treeConfig);
+            });
+        }, true);
+        $scope.$watch(() => (self.selectionType), () => {
+            getSelectionList(self.selectionType, self.treeConfig);
+        })
         self.wells = self.wells || [];
         self.selectionType = self.selectionType || 'family-group';
         if (self.token)
             wiToken.setToken(self.token);
-        getTree();
+        // getTree();
     }
+    function getSelectionList(selectionType, wellArray) {
+        console.log(wellArray);
+        let selectionHash = {};
+        let allCurves = [];
+        wellArray.forEach(well => {
+            let curvesInWell = getCurvesInWell(well);
+            allCurves.push(...curvesInWell);
+        });
+        switch(selectionType) {
+            case 'curve':
+                allCurves.forEach(curve => {
+                    selectionHash[curve.name] = 1;
+                })
+                self.selectionList = Object.keys(selectionHash);
+                break;
+            case 'family': 
+                allCurves.forEach(curve => {
+                    let family = wiApi.getFamily(curve.idFamily);
+                    if(family)
+                        selectionHash[family.name] = 1;
+                })
+                self.selectionList = Object.keys(selectionHash);
+                break;
+            case 'family-group':
+                allCurves.forEach(curve => {
+                    let family = wiApi.getFamily(curve.idFamily);
+                    if(family)
+                        selectionHash[family.familyGroup] = 1;
+                })
+                self.selectionList = Object.keys(selectionHash);
+                break;
+        }
 
-
+    }
 
 
     this.runMatch = function (node, criteria) {
@@ -68,22 +118,22 @@ function multiWellHistogramController($scope, $timeout, $element, wiToken, wiApi
         self.selectedNode = node;
     }
     self.refresh = getTree;
-    function getTree() {
-        self.treeConfig = [];
-        for (let w of self.wells) {
-            wiApi.getWellPromise(w).then(well => {
-                self.treeConfig.push(well);
-                $timeout(() => {});
-            }).catch(e => console.error(e));
-        }
-        /*wiApi.getWellsPromise(self.idProject)
-            .then(wells => $timeout(
-                () => self.treeConfig = wells.sort(
-                    (w1, w2) => (w1.name.localeCompare(w2.name))
-                )
-            ))
-            .catch(err => console.error(err));
-        */    
+    function getTree(callback) {
+        (async function() {
+            self.treeConfig = [];
+            try {
+                for (let w of self.wells) {
+                    let well = await wiApi.getWellPromise(w);
+                    self.treeConfig.push(well);
+                    // $timeout(() => {});
+                }
+            }
+            catch(e) {
+                console.error(e);
+            }
+            $timeout(() => {});
+            callback && callback();
+        })();
     }
 
 
