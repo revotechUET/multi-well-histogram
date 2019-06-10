adf8cd19451fedf2719b5a4c180f186e84a1a991
Hung <hungnk1996@gmail.com>
2019-06-10 19:01:44 +0700

    update

--- src/multi-well-histogram/index.js
+++ src/multi-well-histogram/index.js
@@ -27,7 +27,7 @@ app.component(componentName, {
     transclude: true
 });
 
-function multiWellHistogramController($scope, $timeout, $element, wiToken, wiApi, wiDialog, wiLoadinggit) {
+function multiWellHistogramController($scope, $timeout, $element, wiToken, wiApi, wiDialog, wiLoading) {
     let self = this;
     self.treeConfig = [];
     self.selectedNode = null;
