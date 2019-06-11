8f758d0422174eff0c5f6353a681157d8fcb0038
phucnguyenbn9x <phucnguyenbn.9x@gmail.com>
2019-06-11 16:17:25 +0700

    update

--- src/multi-well-histogram/index.js
+++ src/multi-well-histogram/index.js
@@ -111,8 +111,6 @@ function multiWellHistogramController($scope, $timeout, $element, wiToken, wiApi
         self.zoneTree = [];
         self.zonesetName = self.zonesetName || "ZonationAll";
         self.config = self.config || {grid:true, displayMode: 'bar', colorMode: 'zone', stackMode: 'well', binGap: 5};
-		self.assetInfo = await wiApi.getAssetPromise(self.idHistogram);
-		self.config.title = self.assetInfo.name;
     }
 
     this.onInputSelectionChanged = function(selectedItemProps) {
@@ -684,7 +682,7 @@ function multiWellHistogramController($scope, $timeout, $element, wiToken, wiApi
 					zonesetName: self.zonesetName,
 					selectionType: self.selectionType,
 					selectionValue: self.selectionValue,
-					config: self.config	
+					config: {...self.config, title: name}	
 				}
 				wiApi.newAssetPromise(self.idProject, name, type, content).then(res => {
 					self.setConfigTitle(null, name);
